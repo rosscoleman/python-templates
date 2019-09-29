8e48b9eb17d6e91a9e7c038264efa65284e94927
Ross Coleman <rossco85@gmail.com>
2019-09-28 23:12:36 -0500

    Vagrant Miniconda in PATH (.bashrc)

---
+++ vagrant-miniconda/download-miniconda.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sha256sum ~/miniconda.sh
--- vagrant-miniconda/install-miniconda.sh
+++ vagrant-miniconda/install-miniconda.sh
@@ -1,10 +1,7 @@
+#!/bin/sh
 echo I am provisioning...
 date > /etc/vagrant_provisioned_at
-#sudo apt-add-repository multiverse
-sudo apt-get update
-sudo apt-get install -y wget build-essential
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-sha256sum miniconda.sh
-bash ~/miniconda.sh -b -p /usr/local/share/miniconda
-export PATH="/usr/local/share/miniconda/bin:$PATH"
+bash /home/vagrant/miniconda.sh -b -p /usr/local/share/miniconda
+
+echo "PATH=\"/usr/local/share/miniconda/bin:\$PATH\"" >> /home/vagrant/.bashrc
