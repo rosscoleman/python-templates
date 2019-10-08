17e1b9b83f199c1f22eba953401776ef51665c28
Ross Coleman <rossco85@gmail.com>
2019-10-07 23:04:17 -0500

    data science conda environment on Ubuntu 16.04

--- vagrant-miniconda/download-miniconda.sh
+++ vagrant-miniconda/download-miniconda.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sha256sum ~/miniconda.sh
+# Git Bash on Windows 10 has curl but not wget
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > Miniconda3-latest-Linux-x86_64.sh
+sha256sum ~/Miniconda3-latest-Linux-x86_64.sh
